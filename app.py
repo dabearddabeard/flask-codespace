c81c5fb0a7d3eae4570661507a9f8fefed0c1d41
dabearddabeard <dabeard@nsa.gov>
2023-02-28 20:48:44 +0000

    now

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def arctic_frenz():
+    return render_template("index.html", title="Arctic Frenz")
