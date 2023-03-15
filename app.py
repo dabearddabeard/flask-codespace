3c530833d23b9cd8a658f272391af342dea77148
dabearddabeard <dabeard@nsa.gov>
2023-03-15 19:11:03 +0000

    now

--- app.py
+++ app.py
@@ -1,65 +1,56 @@
-import json
 from flask import Flask, render_template, request
 from datetime import datetime
+import json
 import time
 import schedule
 import requests
-app = Flask(__name__)
-def update_data():
-    url = "https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&api_key=355dbaeffaa15832d7efde34d6a0473a9a6a2c8e0dc54e05b3e6028836e4ba59"
-    response = requests.get(url)
-    data = response.json()
-    with open('static/data.json', 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file)
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
-def index():
-    if request.method == 'POST':
-        with open('static/file1.json', encoding='utf-8') as f1, open('static/file2.json', encoding='utf-8') as f2, open('static/file3.json', encoding='utf-8') as f3, open('static/file4.json', encoding='utf-8') as f4, open('static/file5.json', encoding='utf-8') as f5:
-            data1 = json.load(f1)
-            data2 = json.load(f2)
-            data3 = json.load(f3)
-            data4 = json.load(f4)
-            data5 = json.load(f5)
-            data_list = [data1, data2, data3, data4, data5]
-            if request.form.get('index_value'):
-                index_value = int(request.form.get('index_value'))
-            else:
-                index_value = 0
-            # increment the index
-            index_value = index_value + 1
-            # if the index is greater than the length of the list, reset to 0
-            if index_value > len(data_list) - 1:
-                index_value = 0
-            # set the button text
-            if index_value == 0:
-                button_text = 'AI-1'
-            elif index_value == 1:
-                button_text = 'AI-2'
-            elif index_value == 2:
-                button_text = 'AI-3'
-            elif index_value == 3:
-                button_text = 'AI Plus'
-            elif index_value == 4:
-                button_text = 'Reset'
-            # render the template with the new index
-            return render_template('index.html', data=data_list[index_value], index_value=index_value, button_text=button_text)
-    else:
-        # open the first json file
-        with open('static/file1.json', encoding='utf-8') as f1:
-            # load the json file
-            data1 = json.load(f1)
-            # set the button text
-            button_text = 'AI-1'
-            # render the template with the initial index
-            return render_template('index.html', data=data1, index=0, button_text=button_text)
+def update_content():
+    url = "https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&api_key=355dbaeffaa15832d7efde34d6a0473a9a6a2c8e0dc54e05b3e6028836e4ba59"
+    response = requests.get(url, timeout=10)
+    data = response.json()
+    with open('static/file1.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file)
 
+
+
+def index():
+    index_value = 0
+    button_text = 'AI-1'
+    data_list = []
+    for i in range(1, 6):
+        with open(f'static/file{i}.json', encoding='utf-8') as f:
+            data_list.append(json.load(f))
+    if request.method == 'POST':
+        index_value = int(request.form.get('index_value', 0))
+        index_value = (index_value + 1) % len(data_list)
+        button_text = ['AI-1', 'AI-2', 'AI-3', 'AI Plus', 'Reset'][index_value]
+    data = data_list[index_value]
+    last_set = data['Data']['Data'][-9:]
+    time_close = []
+    for item in last_set:
+        local_date = datetime.fromtimestamp(item["time"]).strftime('%d-%b')
+        local_time = datetime.fromtimestamp(item["time"]).strftime('%H:%M')
+        time_close.append({"date": local_date, "time": local_time, "close": item["close"]})
+    return render_template('index.html', data=time_close, index_value=index_value, button_text=button_text)
+
+
+
+def check_time():
+    with open('static/file1.json', 'r') as json_file:
+        data = json.load(json_file)
+        time_to = data['TimeTo']
+        current_time = time.time()
+        if current_time - time_to > 12 * 60 * 60:
+            update_content()
 
 if __name__ == '__main__':
-    schedule.every(12).hours.do(update_data)
+    # Run the check_time() function every minute
+    schedule.every(1).minutes.do(check_time)
     while True:
         schedule.run_pending()
-        app.run()
         time.sleep(1)
